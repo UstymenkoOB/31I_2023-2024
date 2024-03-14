9c0018fc76484d7cebf5992f9d2f525ce9499f29
ValentynaSavchenko <149622417+ValentynaSavchenko@users.noreply.github.com>
2024-03-14 17:55:42 +0200

    Add files via upload

---
+++ 16_Lab7V2.py
@@ -0,0 +1,28 @@
+from turtle import *
+
+speed(0)
+for i in range(6):
+    color('violet')
+    pensize(15)
+    circle(40)
+    color('blue')
+    pensize(20)
+    circle(50)
+    color('dodgerblue')
+    pensize(25)
+    circle(60)
+    color('green')
+    pensize(30)
+    circle(70)
+    color('yellow')
+    pensize(35)
+    circle(80)
+    color('orange')
+    pensize(40)
+    circle(90)
+    color('red')
+    pensize(45)
+    circle(105)
+    right(60)
+
+mainloop()
